39ee63e2dca4a48f5605ddfa99f22844ab3271d1
kyuyueonyy <rbddl3470@naver.com>
2025-06-24 17:51:31 +0900

    250624 apiserver

---
+++ Spring/security-apiserver/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -0,0 +1,145 @@
+package org.scoula.security.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+@MapperScan(basePackages = {"org.scoula.security.account.mapper"})
+@ComponentScan(basePackages = {"org.scoula.security"})
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    // 문자셋 필터
+    public CharacterEncodingFilter encodingFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(encodingFilter(), CsrfFilter.class);
+
+        //  HTTP 보안 설정
+        http.httpBasic().disable()      // 기본 HTTP 인증 비활성화
+                .csrf().disable()           // CSRF 보호 비활성화 (REST API에서는 불필요)
+                .formLogin().disable()      // 폼 로그인 비활성화 (JSON 기반 API 사용)
+                .sessionManagement()        // 세션 관리 설정
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);  // 무상태 모드
+
+        // 경로별, 접근 권한 설정
+        http.authorizeRequests()
+                .antMatchers("/security/all").permitAll() // 모든권한 접근 허용
+                .antMatchers("/security/admin").access("hasRole('ROLE_ADMIN')") // ROLE_ADMIN만 접근 가능
+                .antMatchers("/security/member").access("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')"); // ROLE_ADMIN, ROLE_MEMBER 접근 가능
+
+        http.formLogin()
+                .loginPage("/security/login") // 사용자가 보게될 로그인 페이지 ( 우리가만든거!)
+                .loginProcessingUrl("/security/login") // 로그인 폼에서 제출되는 URL(POST)
+                .defaultSuccessUrl("/");
+
+        http.logout()
+                .logoutUrl("/security/logout") // -> Spring Security에서 로그아웃 요청을 받는 POST API
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSION-ID")
+                .logoutSuccessUrl("/security/logout"); // GET logout 페이지로 전환
+
+        http.sessionManagement()
+                /*회원 1명당 세션 1개 유지*/
+                .maximumSessions(1)                        // 동시 세션 수 제한
+                .maxSessionsPreventsLogin(false)           // 새 로그인시 기존 세션 만료
+                .expiredUrl("/security/login?expired");    // 세션 만료시 리다이렉트
+
+        http.rememberMe()
+                .key("uniqueAndSecret")                    // 🔑 암호화 키
+                .tokenValiditySeconds(86400)               // ⏰ 24시간 유효
+                .userDetailsService(userDetailsService);   // 👤 사용자 정보 서비스
+
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        log.info("configure .........................................");
+//        // inMemoryAuthentication -> 메모리상에 user정보를 임의로 등록
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+
+    /// /                .password("{noop}1234") // {noop} -> security는 기본적으로 비밀번호 암호화 필수 -> 예외
+//                .password("$2a$10$EsIMfxbJ6NuvwX7MDj4WqOYFzLU9U/lddCyn0nic5dFo3VfJYrXYC")
+//                .roles("ADMIN", "MEMBER"); // ROLE_ADMIN
+//
+//        auth.inMemoryAuthentication()
+//                .withUser("member")
+//                .password("$2a$10$EsIMfxbJ6NuvwX7MDj4WqOYFzLU9U/lddCyn0nic5dFo3VfJYrXYC")
+//                .roles("MEMBER"); // ROLE_MEMBER
+//
+//    }
+
+    // 직접 만든 userDetailsService를 이용해 인증을 진행하도록 설정
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        log.info("configure .........................................");
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        // CORS 설정 객체 생성
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //모든 요청 허용
+        configuration.setAllowedOriginPatterns(List.of("*"));
+//   configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173"));
+
+
+        // 허용할 HTTP 메서드 목록 지정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+        // 모든 요청 헤더 허용
+        configuration.setAllowedHeaders(List.of("*"));
+
+        // 자격 증명(쿠키, Authorization 헤더 등)을 포함한 요청 허용
+        configuration.setAllowCredentials(true);
+
+        // 특정 URL 경로 패턴에 대해 위의 CORS 설정을 적용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 적용
+
+        // 설정된 CORS 소스를 반환 (스프링 시큐리티나 필터 체인에 의해 사용됨)
+        return source;
+    }
+
+}
---
+++ Spring/security-apiserver/src/main/java/org/scoula/security/util/JwtProcessor.java
@@ -0,0 +1,95 @@
+package org.scoula.security.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+//JWT를 사용할 때 필요한 메서드들을 만들어놓기 위한 클래스
+@Component
+@Log4j2
+public class JwtProcessor {
+    //토큰 만료 시간(5분)
+    static private final long TOKEN_VALID_MILLISECOND = 1000L * 60 * 5;
+
+    //토큰 생성 시 필요한 secretKey
+    private final String secretkey = "KB_IT's YOUR LIFE_6기_19회차_JWT수업";
+    //이 secretkey 문자열을 byte배열로 변환해서 넣어줘야함
+    private final Key key = Keys.hmacShaKeyFor(secretkey.getBytes(StandardCharsets.UTF_8));
+
+    //토큰 생성하기
+    //subject : 사용자 (username)
+    public String generateToken(String subject, String role) {
+        return Jwts.builder()
+                .setSubject(subject) //사용자 식별자
+                .setIssuedAt(new Date()) //발급시간
+                .setExpiration(new Date(new Date().getTime() + TOKEN_VALID_MILLISECOND)) //만료시간(현재시간에 토큰 만료시간(5분)을 넣어서)
+                .claim("role", role) // 커스텀 claim 넣는 방법
+                .signWith(key) //서명
+                .compact(); //문자열 생성
+    }
+
+    //검증
+    //subject -> 사용자 식별자 (사용자 정보 중 고유한 값)
+    public String getUserName(String token) {
+
+        //ParserBuilder를 이용해 JwtParser 객체 생성
+        JwtParser jwtParser = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+
+        //토큰 파싱 JWS(JSON Web Signature) (JWT -> 서명된 상태 Signed Token) 구조
+        Jws<Claims> jwsClaims = jwtParser.parseClaimsJws(token);
+
+        //Claims (Payload)부분
+        Claims claims = jwsClaims.getBody();
+
+        //Claims 에서 subject 필드 가져오기
+        String username = claims.getSubject();
+
+        return username;
+    }
+
+    /**
+     * JWT Subject(role) 추출
+     *
+     * @param token JWT 토큰
+     * @return 사용자명
+     * @throws JwtException 토큰 해석 불가 시 예외 발생
+     */
+    public String getRole(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
+    }
+
+
+    /**
+     * JWT 검증 (유효 기간 및 서명 검증)
+     *
+     * @param token JWT 토큰
+     * @return 검증 결과 (true: 유효, false: 무효)
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            //parseClaimsJws -> 서명(Signature) exp(만료일) 검증해줌
+            //사용자 정의 Claim -> 개발자가 따로 검증로직을 만들어주어야함.
+            return true;
+        } catch (Exception e) {
+            log.error("JWT 검증 실패: {}", e.getMessage());
+            return false;
+        }
+    }
+}
+
