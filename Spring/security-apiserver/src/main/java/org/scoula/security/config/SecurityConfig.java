d3a5fa6371cfd4cf956964fbafba3955b737499c
kyuyueonyy <rbddl3470@naver.com>
2025-06-25 15:57:44 +0900

    06/25 -> security-apiserver3

--- Spring/security-apiserver/src/main/java/org/scoula/security/config/SecurityConfig.java
+++ Spring/security-apiserver/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -3,11 +3,16 @@ package org.scoula.security.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.annotation.MapperScan;
+import org.scoula.security.filter.AuthenticationErrorFilter;
+import org.scoula.security.filter.JwtAuthenticationFilter;
 import org.scoula.security.filter.JwtUsernamePasswordAuthenticationFilter;
+import org.scoula.security.handler.CustomAccessDeniedHandler;
+import org.scoula.security.handler.CustomAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,7 +31,6 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import java.util.Arrays;
 import java.util.List;
 
-
 @Configuration
 @EnableWebSecurity
 @Log4j2
@@ -36,16 +40,18 @@ import java.util.List;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthenticationErrorFilter authenticationErrorFilter;
+
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
     // 커스텀 인증 필터 추가
     @Autowired
     private JwtUsernamePasswordAuthenticationFilter jwtUsernamePasswordAuthenticationFilter;
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        log.info("configure .........................................");
-//        // inMemoryAuthentication -> 메모리상에 user정보를 임의로 등록
-//        auth.inMemoryAuthentication()
-//                .withUser("admin")
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     // 문자셋 필터
     public CharacterEncodingFilter encodingFilter() {
@@ -62,10 +68,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //http.addFilterBefore(encodingFilter(), CsrfFilter.class);
         http
-                // API 로그인 인증 필터 추가 (기존 UsernamePasswordAuthenticationFilter 앞에 배치)
-                .addFilterBefore(jwtUsernamePasswordAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//                .addFilterBefore(encodingFilter(), CsrfFilter.class);
+                // token 인증 필터
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+
+                // login 필터
+                .addFilterBefore(
+                        jwtUsernamePasswordAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+
+                // 토큰 인증시 발생하는 예외 처리 필터
+                .addFilterBefore(authenticationErrorFilter, JwtAuthenticationFilter.class)
+
+                // 예외 처리 설정
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)  // 401 에러 처리
+                .accessDeniedHandler(accessDeniedHandler);           // 403 에러 처리
 
         //  HTTP 보안 설정
         http.httpBasic().disable()      // 기본 HTTP 인증 비활성화
@@ -75,69 +95,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);  // 무상태 모드
 
 
-        // 경로별, 접근 권한 설정
-        http.authorizeRequests()
-                .antMatchers("/security/all").permitAll() // 모든권한 접근 허용
-                .antMatchers("/security/admin").access("hasRole('ROLE_ADMIN')") // ROLE_ADMIN만 접근 가능
-                .antMatchers("/security/member").access("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')"); // ROLE_ADMIN, ROLE_MEMBER 접근 가능
-
-        http.formLogin()
-                .loginPage("/security/login") // 사용자가 보게될 로그인 페이지 ( 우리가만든거!)
-                .loginProcessingUrl("/security/login") // 로그인 폼에서 제출되는 URL(POST)
-                .defaultSuccessUrl("/");
-
-
-        http.sessionManagement()
-                /*회원 1명당 세션 1개 유지*/
-                .maximumSessions(1)                        // 동시 세션 수 제한
-                .maxSessionsPreventsLogin(false)           // 새 로그인시 기존 세션 만료
-                .expiredUrl("/security/login?expired");    // 세션 만료시 리다이렉트
+        http
+                .authorizeRequests() // 경로별 접근 권한 설정
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/api/security/all").permitAll()                    // 모두 허용
+                .antMatchers("/api/security/member").access("hasRole('ROLE_MEMBER')")  // ROLE_MEMBER 이상
+                .antMatchers("/api/security/admin").access("hasRole('ROLE_ADMIN')")    // ROLE_ADMIN 이상
+                .anyRequest().authenticated(); // 나머지는 로그인 필요
 
     }
 
-    /// /                .password("{noop}1234") // {noop} -> security는 기본적으로 비밀번호 암호화 필수 -> 예외
-//                .password("$2a$10$EsIMfxbJ6NuvwX7MDj4WqOYFzLU9U/lddCyn0nic5dFo3VfJYrXYC")
-//                .roles("ADMIN", "MEMBER"); // ROLE_ADMIN
-//
-//        auth.inMemoryAuthentication()
-//                .withUser("member")
-//                .password("$2a$10$EsIMfxbJ6NuvwX7MDj4WqOYFzLU9U/lddCyn0nic5dFo3VfJYrXYC")
-//                .roles("MEMBER"); // ROLE_MEMBER
-//
-//    }
 
     // 직접 만든 userDetailsService를 이용해 인증을 진행하도록 설정
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        log.info("configure .........................................");
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
 
-    // AuthenticationManager 빈 등록
+    // AuthenticationManger
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
-
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         // CORS 설정 객체 생성
         CorsConfiguration configuration = new CorsConfiguration();
 
-        //모든 요청 허용
+        // 모든 요청 허용
         configuration.setAllowedOriginPatterns(List.of("*"));
-//   configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173"));
-
+        //  configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173"));
 
         // 허용할 HTTP 메서드 목록 지정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 
         // 모든 요청 헤더 허용
         configuration.setAllowedHeaders(List.of("*"));
-
         // 자격 증명(쿠키, Authorization 헤더 등)을 포함한 요청 허용
         configuration.setAllowCredentials(true);
 
